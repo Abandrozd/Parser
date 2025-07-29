53c3e188a6a84cf0d1c0f0ecccf920b5ee644a92
Abandrozd <58365637+Abandrozd@users.noreply.github.com>
2025-07-29 06:01:37 +0300

    Add files via upload

---
+++ file.py
@@ -0,0 +1,15 @@
+genres = {
+    "https://litnet.com/ru/top/boevoe-fentezi": "Бытовое фентези",
+    "https://litnet.com/ru/top/istoricheskoe-fentezi": "Историческое фентези",
+    "https://litnet.com/ru/top/gorodskoe-fentezi": "Городское фентези",
+    "https://litnet.com/ru/top/priklyuchencheskoe-fentezi": "Приключенческое фентези",
+    "https://litnet.com/ru/top/yumoristicheskoe-fentezi": "Юмористическое фентези",
+    "https://litnet.com/ru/top/bytovoe-fentezi": "Бытовое фентези",
+    "https://litnet.com/ru/top/epicheskoe-fentezi": "Эпическое фентези",
+    "https://litnet.com/ru/top/magicheskaya-akademiya": "Магическая академия",
+    "https://litnet.com/ru/top/aziatskoe-fentezi": "Азиасткое фентези",
+    "https://litnet.com/ru/top/slavyanskoe-fentezi": "Славянское фентези",
+    "https://litnet.com/ru/top/temnoe-fentezi": "Тёмное фентези"
+}
+
+print(genres)
